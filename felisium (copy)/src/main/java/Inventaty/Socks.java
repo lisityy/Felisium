629852d17734c20c0e8e169b8f748c25bd022aab
Pimenova, Olga <pimenol1@fel.cvut.cz>
2023-05-09 20:41:22 +0200

    del copy

--- felisium (copy)/src/main/java/Inventaty/Socks.java
+++
@@ -1,7 +0,0 @@
-package Inventaty;
-
-public class Socks extends Things{
-    public Socks(String s, int i) {
-        super(s, i);
-    }
-}
--- felisium (copy)/src/main/java/Inventaty/Valeriana.java
+++
@@ -1,7 +0,0 @@
-package Inventaty;
-
-public class Valeriana extends Things{
-    public Valeriana(String s, int i) {
-        super(s, i);
-    }
-}
